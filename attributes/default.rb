d8eea452c087cb22eca2fd2650dad5bc7b2f8d3b
Maxim Kozinenko <mkozin@softserveinc.com>
2016-09-12 19:18:09 +0300

    added etcd_test

--- attributes/default.rb
+++ attributes/default.rb
@@ -12,17 +12,14 @@ default['docker']['storage_driver'] = 'overlay'
 # etcd attributes
 default['etcd']['peer_port'] = '2380'
 # iterate through members to create reusable cluster_url string
-unless Chef::Config[:solo]
-  search(:node, 'recipes:"kubernetes::etcd"') do |s|
+if Chef::Config[:solo]
+  default['etcd']['cluster_url'] = 'default=http://127.0.0.1' + ':' + default['etcd']['peer_port']
+else
+  search(:node, 'recipes:kubernetes\:\:etcd') do |s|
     default['etcd']['members'] << s[:fqdn]
   end
-  default['etcd']['members'].each_with_index do |node, idx|
-    if idx < (default['etcd']['members'].size - 1)
-      default['etcd']['cluster_url'] += 'http://' + node + ':' + default['etcd']['peer_port'] + ','
-    else
-      default['etcd']['cluster_url'] += 'https://' + node + ':' + default['etcd']['peer_port']
-    end
+  default['etcd']['members'].each_with_index do |node_name, idx|
+    default['etcd']['cluster_url'] += node_name + '=http://' + node_name + ':' + default['etcd']['peer_port'] + ',' if idx < (default['etcd']['members'].size - 1)
+    default['etcd']['cluster_url'] += node_name + '=https://' + node_name + ':' + default['etcd']['peer_port']
   end
-else
-  default['etcd']['cluster_url'] = 'http://localhost' + default['etcd']['peer_port']
 end
