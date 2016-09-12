3e185eb6ef907380b83d032be31faa026a2ee075
Maxim Kozinenko <mkozin@softserveinc.com>
2016-09-12 23:16:44 +0300

    fixed search for etcd nodes

--- attributes/default.rb
+++ attributes/default.rb
@@ -11,18 +11,3 @@ default['docker']['storage_driver'] = 'overlay'
 
 # etcd attributes
 default['etcd']['peer_port'] = '2380'
-# iterate through members to create reusable cluster_url string
-if Chef::Config[:solo]
-  default['etcd']['cluster_url'] = 'default=http://127.0.0.1' + ':' + default['etcd']['peer_port']
-else
-  search(:node, 'recipes:kubernetes\:\:etcd') do |s|
-    default['etcd']['members'] << "#{s.fqdn}"
-  end
-  default['etcd']['members'].each_with_index do |node_string, idx|
-    if idx < (default['etcd']['members'].size - 1)
-      default['etcd']['cluster_url'] += node_string + '=http://' + node_string + ':' + default['etcd']['peer_port'] + ','
-    else
-      default['etcd']['cluster_url'] += node_string + '=https://' + node_string + ':' + default['etcd']['peer_port']
-    end
-  end
-end
--- recipes/etcd.rb
+++ recipes/etcd.rb
@@ -7,6 +7,23 @@
 # All rights reserved - Do Not Redistribute
 #
 
+# iterate through members to create reusable cluster_url string
+if Chef::Config[:solo]
+  cluster_url = 'default=http://127.0.0.1' + ':' + node['etcd']['peer_port']
+else
+  etcd_members = []
+  search(:node, 'recipes:kubernetes\:\:etcd') do |s|
+    etcd_members << s[:fqdn]
+  end
+  etcd_members.each_with_index do |node_string, idx|
+    if idx < (etcd_members.size - 1)
+      cluster_url += node_string + '=http://' + node_string + ':' + node['etcd']['peer_port'] + ','
+    else
+      cluster_url += node_string + '=https://' + node_string + ':' + node['etcd']['peer_port']
+    end
+  end
+end
+
 if Chef::Config[:solo]
   etcd_service 'default' do
     advertise_client_urls 'http://127.0.0.1:2379,http://127.0.0.1:4001'
@@ -14,7 +31,7 @@ if Chef::Config[:solo]
     initial_advertise_peer_urls 'http://127.0.0.1:2380'
     listen_peer_urls 'http://0.0.0.0:2380'
     initial_cluster_token 'etcd-cluster-1'
-    initial_cluster node['etcd']['cluster_url']
+    initial_cluster cluster_url
     initial_cluster_state 'new'
     action [:create, :start]
   end
@@ -25,7 +42,7 @@ else
     initial_advertise_peer_urls 'http://127.0.0.1:2380'
     listen_peer_urls 'http://0.0.0.0:2380'
     initial_cluster_token 'etcd-cluster-1'
-    initial_cluster node['etcd']['cluster_url']
+    initial_cluster cluster_url
     initial_cluster_state 'new'
     action [:create, :start]
   end
