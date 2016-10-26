e5cb5eba85af7c1c16a66ce1d8709540e564a01e
Maxim Kozinenko <mkozin@softserveinc.com>
2016-10-26 21:17:43 +0300

    using databag as tokens storage after vault_init

--- recipes/vault-init.rb
+++ recipes/vault-init.rb
@@ -14,5 +14,31 @@ ruby_block 'vault_init' do
     Chef::Resource::RubyBlock.send(:include,Chef::Mixin::ShellOut)
     init_cmd = shell_out('vault init').stdout
     # save Keys and token as DataBag items here
+
+    init_cmd_data = {
+      'id' => 'vault',
+      'keys'  => [],
+      'token' => ''
+    }
+
+    init_cmd.each_line do |line|
+      if line.start_with?('Key ')
+        key = line.split(':')[1].strip
+        init_cmd_data['keys'] << key
+      end
+
+      if line.start_with?('Initial Root Token')
+        token = line.split(':')[1].strip
+        init_cmd_data['token'] = token
+      end
+    end
+    databag = Chef::DataBag.new
+    databag.name('vaultkeys')
+    databag.create
+    databag_item = Chef::DataBagItem.new
+    databag_item.raw_data = init_cmd_data
+    databag_item.data_bag('vaultkeys')
+    databag_item.create
+    databag_item.save
   end
 end
