53ade373c77e59e76b0d343484fa83a4c7415597
Maxim Kozinenko <mkozin@softserveinc.com>
2016-10-28 16:09:04 +0300

    pki configuration recipes

--- recipes/vault-conf-pki.rb
+++ recipes/vault-conf-pki.rb
@@ -9,7 +9,17 @@
 
 ENV['VAULT_ADDR'] = 'https://' + node['ipaddress'] + ':8200'
 
-if !File.file?('/etc/vault_ca/ca.pem')
+directory '/etc/vault_ca' do
+  recursive true
+end
+
+if !File.file?('/etc/pki/ca-trust/source/anchors/vault_ca.pem')
+  # auth with vault
+  token = data_bag_item('vault_keys', 'vault_token')['token']
+  execute 'vault_auth' do
+    command "vault auth #{token}"
+  end
+
   # create and configure root CA
   execute 'mount_root_pki' do
     command 'vault mount -path=k8s-infra -description="K8S cluster Root CA" -max-lease-ttl=87600h pki'
@@ -30,17 +40,27 @@ if !File.file?('/etc/vault_ca/ca.pem')
 
   ruby_block 'create_intermediate_ca' do
     block do
-      request = shell_out('vault write k8s-infra-interm/intermediate/generate/internal common_name="K8S cluster Intermediate CA" ttl=26280h key_bits=4096 exclude_cn_from_sans=true').stdout
-      csr_text = request.split('csr')[1].strip
-      sign_request = shell_out("vault write k8s-infra-interm/root/sign-intermediate csr=#{csr_text} common_name='K8S cluster Intermediate CA ttl=8760h").stdout
-      
+      csr_request = shell_out('vault write -format=json k8s-infra-interm/intermediate/generate/internal common_name="K8S cluster Intermediate CA" ttl=26280h key_bits=4096 exclude_cn_from_sans=true').stdout
+      csr_text = JSON.parse(csr_request)
+      shell_out("vault write -format=json k8s-infra-interm/root/sign-intermediate csr=#{csr_text} common_name='K8S cluster Intermediate CA ttl=8760h").stdout
     end
   end
-  ruby_block 'create_root_ca' do
-    block do
-      Chef::Resource::RubyBlock.send(:include,Chef::Mixin::ShellOut)
-      raw_cert_data = shell_out('vault write k8s-infra/root/generate/internal common_name="K8S cluster Root CA" ttl=87600h key_bits=4096 exclude_cn_from_sans=true').stdout
 
-    end
+  execute 'configure_intermediate_ca_urls' do
+    command "vault write k8s-infra-interm/config/urls issuing_certificates=\"https://#{node['ipaddress']}:8200/v1/k8s-infra-interm/ca\" crl_distribution_points=\"https://#{node['ipaddress']}:8200/v1/cuddletech_ops/crl\""
   end
+
+  # configure cert roles
+  execute 'configure_etcd_role' do
+    command 'vault write k8s-infra-interm/roles/server key_bits=2048 max_ttl=8760h allow_any_name=true'
+  end
+
+  remote_file '/etc/pki/ca-trust/source/anchors/vault_ca.pem' do
+    source "https://#{node['ipaddress']}:8200/v1/k8s-infra/ca/pem"
+  end
+
+  execute 'update_trust' do
+    command '/usr/bin/update-ca-trust'
+  end
+
 end
